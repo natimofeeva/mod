23c50701081235a095402b912342b24c886e8936
Dr. Ernesto Lee <socrates73@gmail.com>
2024-12-04 04:14:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,29 +39,5 @@ if st.button('Detect Hate'):
     json_output = json.dumps(serialized_output, indent=2, ensure_ascii=False)
     st.json(json_output)
 
-# Insert containers separated into tabs:
-tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
-tab1.write("this is tab 1")
-tab2.write("this is tab 2")
 
-# You can also use "with" notation:
-with tab1:
-  st.radio('Select one:', [1, 2])
-
-st.button('Hit me')
-st.checkbox('Check me out')
-st.radio('Pick one:', ['nose','ear'])
-st.selectbox('Select', [1,2,3])
-st.multiselect('Multiselect', [1,2,3])
-st.slider('Slide me', min_value=0, max_value=10)
-st.select_slider('Slide to select', options=[1,'2'])
-st.text_input('Enter some text')
-st.number_input('Enter a number')
-st.text_area('Area for textual entry')
-st.date_input('Date input')
-st.time_input('Time entry')
-st.file_uploader('File uploader')
-st.download_button('On the dl', data)
-st.camera_input("一二三,茄子!")
-st.color_picker('Pick a color')
 
